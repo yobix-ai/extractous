a0ab3d00894221e30d8a4fc6086bb9237ea63c24
nmammeri <nmammeri@gmail.com>
2024-07-03 12:55:08 +0200

    ci: add graalvm install script

---
+++ .github/workflows/install-graalvm-linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e -x
+
+curl -s "https://get.sdkman.io" | sh -s -- -y
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk install java 22.0.1-graalce
+sdk default java 22.0.1-graalce
