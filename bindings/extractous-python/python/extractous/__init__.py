8cd1240cb0dfca7e67f4254d34947bb9c7fa5c25
nmammeri <nmammeri@gmail.com>
2024-10-28 20:03:35 +0100

    fix: add python extension dir to the windows PATH to find graalvm libs

--- bindings/extractous-python/python/extractous/__init__.py
+++ bindings/extractous-python/python/extractous/__init__.py
@@ -1,3 +1,12 @@
+import os
+from platform import system
+
+# On Windows there is no equivalent way of setting RPATH
+# This adds the current directory to PATH so that the graalvm libs will be found 
+if system() == "Windows":
+    libpath = os.path.dirname(__file__)
+    os.environ["PATH"] = libpath + os.pathsep + os.environ["PATH"]
+
 from ._extractous import *
 
 __doc__ = _extractous.__doc__
