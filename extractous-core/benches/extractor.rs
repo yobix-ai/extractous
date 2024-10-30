585835623bb7c098e4359636d8d028af4835d699
nmammeri <nmammeri@gmail.com>
2024-10-30 13:29:41 +0100

    fix: add criterion benchmarks

---
+++ extractous-core/benches/extractor.rs
@@ -0,0 +1,40 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+
+use std::io::{BufReader, Read};
+use extractous::Extractor;
+
+fn extract_to_stream(c: &mut Criterion) {
+
+    let file_path = "../test_files/documents/2022_Q3_AAPL.pdf";
+    let extractor = Extractor::new();
+
+    c.bench_function("extract_to_stream", |b| {
+        b.iter(|| {
+
+            // Extract the provided file content to a stream
+            let stream = extractor.extract_file(file_path).unwrap();
+            // Because stream implements std::io::Read trait we can perform buffered reading
+            // For example we can use it to create a BufReader
+            let mut reader = BufReader::new(stream);
+            let mut buffer = Vec::new();
+            reader.read_to_end(&mut buffer).unwrap();
+        })
+    });
+}
+
+fn extract_to_string(c: &mut Criterion) {
+
+    let file_path = "../test_files/documents/2022_Q3_AAPL.pdf";
+    let extractor = Extractor::new();
+
+    c.bench_function("extract_to_string", |b| {
+        b.iter(|| {
+            // Extract the provided file content to a string
+            let _content = extractor.extract_file_to_string(file_path).unwrap();
+        })
+    });
+}
+
+criterion_group!(benches, extract_to_stream, extract_to_string);
+criterion_main!(benches);
