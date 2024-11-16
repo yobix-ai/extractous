5e32273a188c4f9961d71e19c186fce91a3767e7
nmammeri <nmammeri@gmail.com>
2024-11-16 18:52:30 +0100

    tests: add extract_url_to_string and extract_bytes_to_string tests

--- bindings/extractous-python/tests/test_extract_bytes.py
+++ bindings/extractous-python/tests/test_extract_bytes.py
@@ -2,7 +2,8 @@ import json
 import pytest
 
 from extractous import Extractor
-from utils import calculate_similarity_percent, cosine_similarity, read_to_string, read_file_to_bytearray
+from utils import calculate_similarity_percent, cosine_similarity, read_to_string, read_file_to_bytearray, \
+    is_expected_metadata_contained
 
 TEST_CASES = [
     ("2022_Q3_AAPL.pdf", 0.9),
@@ -19,6 +20,34 @@ TEST_CASES = [
     #("eng-ocr.pdf", 0.9),
 ]
 
+
+@pytest.mark.parametrize("file_name, target_dist", TEST_CASES)
+def test_extract_bytes_to_string(file_name, target_dist):
+    """Test the extraction from bytes of various file types."""
+    original_filepath = f"../../test_files/documents/{file_name}"
+    expected_result_filepath = f"../../test_files/expected_result/{file_name}.txt"
+    expected_metadata_result_filepath = f"../../test_files/expected_result/{file_name}.metadata.json"
+
+    # Read expected
+    with open(expected_result_filepath, "r",  encoding="utf8") as file:
+        expected = file.read()
+    with open(expected_metadata_result_filepath, 'r', encoding="utf8") as file:
+        expected_metadata = json.load(file)
+
+    # Extract
+    file_bytes = read_file_to_bytearray(original_filepath)
+
+    extractor = Extractor()
+    result, metadata = extractor.extract_bytes_to_string(file_bytes)
+
+    # Check Expected
+    assert cosine_similarity(result, expected) > target_dist, \
+        f"Cosine similarity is less than {target_dist} for file: {file_name}"
+
+    # Check metadata
+    percent_similarity = calculate_similarity_percent(metadata, expected_metadata)
+    assert percent_similarity > target_dist, \
+        f"The metadata similarity is lower than expected. Current {percent_similarity}% | filename: {file_name}"
 
 @pytest.mark.parametrize("file_name, target_dist", TEST_CASES)
 def test_extract_bytes_to_stream(file_name, target_dist):
--- bindings/extractous-python/tests/test_extract_file.py
+++ bindings/extractous-python/tests/test_extract_file.py
@@ -2,7 +2,7 @@ import json
 import pytest
 
 from extractous import Extractor
-from utils import calculate_similarity_percent, cosine_similarity, is_expected_metadata_contained
+from utils import calculate_similarity_percent, cosine_similarity, is_expected_metadata_contained, read_to_string
 
 TEST_CASES = [
     ("2022_Q3_AAPL.pdf", 0.9),
@@ -43,3 +43,31 @@ def test_extract_file_to_string(file_name, target_dist):
     # Check metadata
     #metadata.pop("dc:format")
     assert is_expected_metadata_contained(expected_metadata, metadata)
+
+
+@pytest.mark.parametrize("file_name, target_dist", TEST_CASES)
+def test_extract_file_to_stream(file_name, target_dist):
+    """Test the extraction from bytes of various file types."""
+    original_filepath = f"../../test_files/documents/{file_name}"
+    expected_result_filepath = f"../../test_files/expected_result/{file_name}.txt"
+    expected_metadata_result_filepath = f"../../test_files/expected_result/{file_name}.metadata.json"
+
+    # Read expected
+    with open(expected_result_filepath, "r",  encoding="utf8") as file:
+        expected = file.read()
+    with open(expected_metadata_result_filepath, 'r', encoding="utf8") as file:
+        expected_metadata = json.load(file)
+
+    # Extract
+    extractor = Extractor()
+    reader, metadata = extractor.extract_file(original_filepath)
+    result = read_to_string(reader)
+
+    # Check Expected
+    assert cosine_similarity(result, expected) > target_dist, \
+        f"Cosine similarity is less than {target_dist} for file: {file_name}"
+
+    # Check metadata
+    percent_similarity = calculate_similarity_percent(metadata, expected_metadata)
+    assert percent_similarity > target_dist, \
+        f"The metadata similarity is lower than expected. Current {percent_similarity}% | filename: {file_name}"
