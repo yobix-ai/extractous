e78358c5a1e7b33050adbf970dc09d6d873fdab0
nmammeri <nmammeri@gmail.com>
2024-12-20 02:07:26 +0100

    fix: added bytes python stream as xml test

--- bindings/extractous-python/tests/test_extract_bytes.py
+++ bindings/extractous-python/tests/test_extract_bytes.py
@@ -107,3 +107,35 @@ def test_extract_bytes_to_stream(file_name, target_dist, metadata_dist):
     percent_similarity = calculate_similarity_percent(metadata, expected_metadata)
     assert percent_similarity >= metadata_dist, \
         f"The metadata similarity is lower than expected. Current {percent_similarity}% | filename: {file_name}"
+
+
+@pytest.mark.parametrize("file_name, target_dist, metadata_dist", TEST_CASES)
+def test_extract_bytes_to_stream_as_xml(file_name, target_dist, metadata_dist):
+    """Test the extraction from bytes to stream as xml of various file types."""
+    original_filepath = f"../../test_files/documents/{file_name}"
+    expected_result_filepath = f"../../test_files/expected_result/{file_name}.txt"
+    expected_metadata_result_filepath = f"../../test_files/expected_result/{file_name}.metadata.json"
+
+    # Read expected
+    with open(expected_result_filepath, "r",  encoding="utf8") as file:
+        expected = file.read()
+    with open(expected_metadata_result_filepath, 'r', encoding="utf8") as file:
+        expected_metadata = json.load(file)
+
+    # Extract
+    file_bytes = read_file_to_bytearray(original_filepath)
+
+    extractor = Extractor()
+    extractor = extractor.set_xml_output(True)
+    reader, metadata = extractor.extract_bytes(file_bytes)
+    result_xml = read_to_string(reader)
+    result_text = extract_body_text(result_xml)
+
+    # Check Expected
+    assert cosine_similarity(result_text, expected) >= target_dist, \
+        f"Cosine similarity is less than {target_dist} for file: {file_name}"
+
+    # Check metadata
+    percent_similarity = calculate_similarity_percent(metadata, expected_metadata)
+    assert percent_similarity >= metadata_dist, \
+        f"The metadata similarity is lower than expected. Current {percent_similarity}% | filename: {file_name}"
